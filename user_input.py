0490b9fb23135c76954dc5a8a701227078afe03c
Logik <99355374+L0gik-hub@users.noreply.github.com>
2024-03-08 23:28:41 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name=input("What is your name ?: ")
+age=input("What is your age ?: ")
+location=input("Where are you living ?: ")
+
+print( "Hello", name, "you are", age, "years old", "and live in", location)
